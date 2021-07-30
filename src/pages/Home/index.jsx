fac6d6b15dc71f48a2d7781168a9e87569402024
jessicacosta07 <jessikacostajf@gmail.com>
2021-07-30 11:19:15 -0300

    ajustes

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Container } from './styled';
+
+const Home = () => <Container>Hello World</Container>
+
+export default Home;
---
+++ src/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+    colors: {
+        primay: '#6200ee',
+        background: '#00000014',
+        text: '#000000B3',
+        sucess: '#135E01',
+    },
+    fonts: {
+        regular: 'Robotom, sans-serif',
+    }
+}
+export default theme;
