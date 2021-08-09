9ac4ba5a46d3c72cea644bbdab684d1caeddc406
jessicacosta07 <jessikacostajf@gmail.com>
2021-08-09 15:56:30 -0300

    parte de react lottie

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Lottie from "react-lottie";
+import animationData from '../../assets/restaurants-loading.json';
+
+export default () => {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
+    };
+    return <Lottie options={defaultOptions} />;
+}
